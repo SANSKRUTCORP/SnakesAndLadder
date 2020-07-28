8fbbfe16cb0de61b0b870a0dc9fc5fb11be5be65
Chinmay Jain <chinmayrjain1@gmail.com>
2020-07-28 21:48:33 +0530

    Merge changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyCDSY4bi1rNZtZpEt3wfqNAyfP8kePSLRg',
+    authDomain: 'snakesandladders-interns2020.firebaseapp.com',
+    databaseURL: 'https://snakesandladders-interns2020.firebaseio.com',
+    projectId: 'snakesandladders-interns2020',
+    storageBucket: 'snakesandladders-interns2020.appspot.com',
+    messagingSenderId: '825100594101',
+    appId: '1:825100594101:web:01b48891ab49cf554c47fc',
+    measurementId: 'G-ZMGHQJ7K0V'
+  }
 };
 
 /*
