6f61600e2b1405dfe0af89d6eec5ae0195c5685a
Chinmay Jain <chinmayrjain1@gmail.com>
2020-07-28 21:56:56 +0530

    Revert "Merge changes"
    This reverts commit 8fbbfe16cb0de61b0b870a0dc9fc5fb11be5be65.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,23 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-// Firebase
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
+
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule,
-    AngularFirestoreModule
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig : {
-    apiKey: 'AIzaSyCDSY4bi1rNZtZpEt3wfqNAyfP8kePSLRg',
-    authDomain: 'snakesandladders-interns2020.firebaseapp.com',
-    databaseURL: 'https://snakesandladders-interns2020.firebaseio.com',
-    projectId: 'snakesandladders-interns2020',
-    storageBucket: 'snakesandladders-interns2020.appspot.com',
-    messagingSenderId: '825100594101',
-    appId: '1:825100594101:web:01b48891ab49cf554c47fc',
-    measurementId: 'G-ZMGHQJ7K0V'
-  }
+  production: true
 };
