2e21bdd2d5acb6a6b60055a1b0b75fb7e6a0429a
Harsh <harsh.here17@gmail.com>
2020-08-11 10:37:53 +0530

    Updated Formula for board

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BoardPageRoutingModule } from './board-routing.module';
 
 import { BoardPage } from './board.page';
@@ -13,6 +13,7 @@ import { BoardPage } from './board.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
     BoardPageRoutingModule
   ],
   declarations: [BoardPage]
