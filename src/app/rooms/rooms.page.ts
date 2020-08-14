e8644839eff2940563ac2bd802bf1538ffb873fc
Ishika <ishikatiwari416@gmail.com>
2020-08-14 18:52:30 +0530

    Add modal to join room option and fix background image of every page

--- src/app/rooms/rooms.page.ts
+++ src/app/rooms/rooms.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
+import { JoinRoomComponent } from './join-room/join-room.component';
+
 
 @Component({
   selector: 'app-rooms',
@@ -8,14 +10,15 @@ import { AlertController } from '@ionic/angular';
 })
 export class RoomsPage implements OnInit {
 
-  constructor(private alertCtrl: AlertController ) {
+  constructor(private modalController: ModalController) {
+
+<<<<<<< HEAD
  }
-
-
+=======
  async showMessage() {
   const alert = await this.alertCtrl.create({
     cssClass: 'my-custom-class',
-    message: 'Password',
+    message: 'Enter Room Joining Code',
     inputs: [
       {
         type: 'password',
@@ -38,7 +41,23 @@ export class RoomsPage implements OnInit {
 message() {
   console.log('ffdv');
 }
+>>>>>>> ebb076f458297ecd860c3368277c02295f24c6c2
 
+  popup() {
+    const modal = this.modalController
+      .create({
+        component: JoinRoomComponent,
+        cssClass: 'my-custom-modal-css',
+        showBackdrop: true,
+        backdropDismiss: true,
+        swipeToClose: true
+      })
+      .then(popElement => {
+        popElement.present(),
+          popElement.onDidDismiss().then(resp => {
+          });
+      });
+  }
 
   ngOnInit() {
   }
