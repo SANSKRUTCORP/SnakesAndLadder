5c369e29eb45947d8918910574a59eef577e4929
Harsh <harsh.here17@gmail.com>
2020-08-15 20:56:23 +0530

    Multiple rooms API generate and updating to DB

--- Node-server/server.js
+++ Node-server/server.js
@@ -20,6 +20,8 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -32,7 +34,18 @@ app.use((req, res, next) => {
     }
 });
 
+
+
 app.use(express.json());
+
+// admin.auth().verifyIdToken(idToken)
+//   .then(function(decodedToken) {
+//     let uid = decodedToken.uid;
+//     // ...
+//   }).catch(function(error) {
+//     // Handle error
+//   });
+
 
 function randomgen(){
     var minimum = 1000000;
@@ -40,10 +53,9 @@ function randomgen(){
     var x = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
     return x;
 };
-
-var count = 1;
 // var currentUser = firebaseAdmin.auth.Userinfo.displayName;
-var currentUser = 'Creator';
+var currentUser = 'its Cipher';
+var count = 2;
 
 
 app.get('/createroom', function(req, res){
@@ -51,28 +63,48 @@ app.get('/createroom', function(req, res){
     var roomToken = randomgen();  
     var roomRef = firedb.ref('/rooms');
     
-    roomRef.child('room_'+roomToken).set({'roomid':roomToken})
+    roomRef.child('room_'+roomToken).set({roomid:roomToken, tempCounter:2})
             .then(function(){
                 console.log(roomToken)
-                res.redirect('/createroom');
+                res.redirect('/createroom/'+roomToken);
             })
             .catch(function(err){
                 console.log(err);
             });
     
     var roomRef1 = firedb.ref('/rooms/room_'+roomToken+'/players');        
-    roomRef1.child('player_'+count).set({name : currentUser})
+    roomRef1.child('player_1').set({name : currentUser})
     .then(function(){
-            count++;
-            res.redirect('/joinroom');
+            
     })
     .catch(function(err){
         console.log(err);
     });
 
-    res.json({roomid : roomToken, room_creator : currentUser});
+    // res.json({roomid : roomToken, room_creator : currentUser});
 
 });
+
+app.get('/createroom/:id', function(req, res){
+    var token = req.params.id ;
+    var roomRef1 = firedb.ref('/rooms/room_'+token+'/players');
+    roomRef1.once("value")
+            .then(function(snapshot){
+                
+                var user2 = snapshot.child('player_2/name');
+                var user3 = snapshot.child('player_3/name');
+                var user4 = snapshot.child('player_4/name');
+                console.log(user2.val(), user3.val(), user4.val())
+
+                res.json({roomid : token,
+                          creator : currentUser,
+                          player2 : user2.val(),
+                          player3 : user3.val(),
+                          player4 : user4.val()})
+            });
+
+})
+
 
 
 
@@ -86,27 +118,38 @@ app.get('/joinroom', function(req, res){
 app.post('/joinroom', function(req, res){
 
     var roomToken = req.body.enterid;
-    var roomRef = firedb.ref('/rooms/room_'+roomToken+'/players');
+    var username = req.body.entername;
+    
+    var ref = firedb.ref('/rooms/room_'+roomToken);
 
+    ref.once('value', function(snapshot){
 
-    roomRef.once('value', function(data){
-                  
-        var lenref = Object.keys(data.val()).length;
+        var count = snapshot.child('tempCounter');
+        var countVal = count.val();
 
-        if(lenref<4){
-            roomRef.child('player_'+count).set({name : 'Harsh'})
-                .then(function(){
-                        count++;
-                        res.redirect('/joinroom');
-                })
-                .catch(function(err){
-                    console.log(err);
-                });
-        } else{
-            res.send('Max players');
-        };
-    });
-   
+        var roomRef = firedb.ref('/rooms/room_'+roomToken+'/players');
+        roomRef.once('value', function(data){
+                    
+            var lenref = Object.keys(data.val()).length;
+
+            if(lenref<4){
+                roomRef.child('player_'+countVal).set({name : username})
+                    .then(function(){
+                            countVal = countVal+1;
+                            ref.update({tempCounter : countVal});
+                            res.redirect('/createroom/'+roomToken);
+                    })
+                    .catch(function(err){
+                        console.log(err);
+                    });
+            } else{
+                res.send('Max players');
+            };
+            
+        });
+    })
+
+ 
 });
 
 app.post('/board', function(req, res){
