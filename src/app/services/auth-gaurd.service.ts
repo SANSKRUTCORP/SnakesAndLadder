c65c0dd8fc4bcef0ffbaf1367dc266537b118527
Harsh <harsh.here17@gmail.com>
2020-09-15 02:02:58 +0530

    changed truth condition

--- src/app/services/auth-gaurd.service.ts
+++ src/app/services/auth-gaurd.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
-import { AuthserviceService } from '../services/authservices.service'
+import { AuthserviceService } from '../services/authservices.service';
 @Injectable()
 export class AuthGuardService implements CanActivate {
   constructor(public auth: AuthserviceService, public router: Router) {}
   canActivate(): boolean {
-    if (!this.auth.isUserSignedIn()) {
+    const token = sessionStorage.getItem('tempid');
+    if (!token) {
       this.router.navigate(['home']);
       return false;
     }
