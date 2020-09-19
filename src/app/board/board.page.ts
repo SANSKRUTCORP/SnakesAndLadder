b7adface734f3e3d385989826814f82d0cd7c0c7
Harsh <harsh.here17@gmail.com>
2020-09-19 13:45:17 +0530

    Changed whole game structure and flow with live db reading

--- src/app/board/board.page.ts
+++ src/app/board/board.page.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AttachSession } from 'protractor/built/driverProviders';
 import { ModalController } from '@ionic/angular';
 import { WinComponent } from './win/win.component';
-import { identifierModuleUrl } from '@angular/compiler';
 import { AngularFireDatabase } from '@angular/fire/database';
-
 import { AllComponent } from './all/all.component';
 import { BoardService } from '../services/board.service';
 import { LoadingController } from '@ionic/angular';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import {CreateroomPage} from '../createroom/createroom.page';
-import {  Observable } from 'rxjs';
-import {interval} from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-board',
@@ -20,202 +14,197 @@ import {interval} from 'rxjs';
   styleUrls: ['./board.page.scss'],
 })
 export class BoardPage implements OnInit {
-   n:number;
-  value: any;
   playerss: any;
-  myName: any;
-  timeVar: any;
-  interval: NodeJS.Timeout;
-  timeLeft: number=10;
-  // posPlayer: number;
-  timeUp:string="time Up"
   diceNumber: any;
   members: any;
-  memberChance: any;
-  posPlayer1:number=0;
-  posPlayer2:number=0;
-  posPlayer3:number=0;
-  posPlayer4:number=0;
-  token1_image:any;
+  memberChance = 1;
   arr: number[][];
-  count=100;
-  v:number;
-  m:number;
-  ladderposition:number;
-  s:number;
-  snakeposition:number;
-  winPos:number;
-  newPos:number;
-  updatePos:number;
-  public data: any;
   player: any;
-  i=1
   mySubscription: any;
-  play=true;
- 
-  
+  play = true;
+  roomToken: any;
+  names = [];
+  positions = [];
+  lengthNames: any;
 
-  
-constructor(private http: HttpClient, 
+constructor(private http: HttpClient,
+            private zone: NgZone,
+            private route: ActivatedRoute,
             private modalController: ModalController,
-             public db : AngularFireDatabase,
-             private boardService:BoardService,
-             public loadingController: LoadingController) {
+            public db: AngularFireDatabase,
+            private boardService: BoardService,
+            public loadingController: LoadingController) {
 
-              
-  this.myName=boardService.getData(); //the data of players we get from create room page
+              // this.myName = boardService.getData(); //the data of players we get from create room page
+              this.roomToken = this.route.snapshot.queryParamMap.get('room');
+              // this.roomToken = 4132139;
 
-   this.arr=[]
-    for(let el = 0; el <10; el++) {
-      this.arr[el]=[]
-        for(let ele = 0; ele<10; ele++){
-            // this.arr[el][ele]=this.count
-            // this.count--;
-            if(el%2==0){
-              this.arr[el][ele] = 101 - (10*el+ele+1);
-          } else{
-              this.arr[el][ele] = (10*(10-el)+1)+10*(10-el-1) - (101 - (10*el+ele+1));
-          }
-        }   
-    }
-    
+              this.arr = [];
+              for (let el = 0; el < 10; el++) {
+                this.arr[el] = [];
+                for (let ele = 0; ele < 10; ele++){
+                    // this.arr[el][ele]=this.count
+                    // this.count--;
+                    if (el % 2 === 0){
+                      this.arr[el][ele] = 101 - (10 * el + ele + 1);
+                  } else{
+                      this.arr[el][ele] = (10 * (10 - el) + 1) + 10 * (10 - el - 1) - (101 - (10 * el + ele + 1));
+                  }
+                }
+              }
+            }
 
-   
-}
+  readPlayers(){
+    const ref = this.db.database.ref('rooms/room_' + this.roomToken + '/players');
+    ref.once('value', snapshot => {
+      this.zone.run(() => {
+        this.lengthNames = Object.keys(snapshot.val()).length;
+        for (let i = 1; i <= this.lengthNames; i++){
+          this.names.push(snapshot.child('player_' + i + '/name').val());
+        }
+      });
+    });
+  }
 
-
-ngOnInit() {
-  console.log(this.myName)
-  this.playersChances();
-  this.presentLoading()
-  
-}
-//this is used to run the for loop of players
-playersChances(){
-  for(this.playerss=this.i;this.playerss<=4;this.playerss++) {
-    this.memberChance =  this.i; 
-    console.log(this.myName[this.i-1]+" "+"turn"); 
-    
-    break;  
- }
- if(this.i<4){
-  this.i =this.i+1; 
-
- }
-  else{
-    this.i=1;   
-  } 
-}
-rollDiceChance(){  
-  /*on every click of dice this function gives one by one chance 
-  to all player to roll it*/  
-  this.boardService.diceRoll().subscribe(resp =>{
-    console.log("dice value",resp);
-    this.diceNumber=resp;
-    this.playersChances();
-    
-},(error)=>{
-    console.log("Error on req : ",error);
-     return null;
-});
- 
-
-}
-//it shows the loading when you start the game.
-async presentLoading() {
-  this.playAudio()
-  const loading = await this.loadingController.create({
-    cssClass: 'my-custom-class',
-  
-    message: 'your game will start in few seconds',
-    duration: 14000
-  });
-  await loading.present();
-
-  console.log('Loading dismissed!');
-}
-
-playAudio(){
-  let audio = new Audio('../assets/starter.mpeg'); //audio play when we start the game
-  audio.play();
-}
-WinnerAudio(){
-  let audio = new Audio('../assets/winner.mp3.wav'); //audio play after wining of player.
-  audio.play();
-}
-diceAudio(){
-  let audio = new Audio('../assets/dice.mp3.wav'); //audio play on dice roll
-  audio.play();
-}
+  ngOnInit() {
+    this.readPlayers();
+    this.presentLoading();
+  }
 
 
-playerPosition(position:number){ 
-  // this function changes player position according to the dicenumber .
-    position=position+this.diceNumber;
-    // this.win(this.posPlayer1)
-      if(position==100){
-        position=100
-        this.play=false;
+  rollDiceChance(){
+    /*on every click of dice this function gives one by one chance
+    to all player to roll it*/
+    this.boardService.diceRoll().subscribe(resp => {
+      console.log('dice value', resp);
+      this.diceNumber = resp;
+      this.playerPosition();
+    }, (error) => {
+        console.log('Error on req : ', error);
+        return null;
+    });
+  }
+
+
+  playerPosition(){
+    // console.log(this.memberChance)
+    const ref = this.db.database.ref('rooms/room_' + this.roomToken + '/players');
+    ref.once('value', snapshot => {
+
+      this.zone.run(() => {
+        const lenref = Object.keys(snapshot.val()).length;
+        for (let i = 1; i <= lenref; i++){
+          this.positions[i - 1] = snapshot.child('player_' + i + '/position').val();
+        }
+      });
+
+      this.db.database.ref('rooms/room_' + this.roomToken).on('value', chance => {
+        this.zone.run(() => {
+          this.memberChance = chance.child('memberChance').val();
+        });
+      });
+
+    }).then(_ => {
+
+      this.positions[this.memberChance - 1] = this.positions[this.memberChance - 1] + this.diceNumber;
+        // this.win(this.positions[this.memberChance - 1]) }
+      if (this.positions[this.memberChance - 1] >= 100){
+        this.positions[this.memberChance - 1] = 100;
+        this.play = false;
         this.popup();
-       console.log("YOU WIN and your position is"+" "+position);
-      }
-       if(position>100){
-          position=position-this.diceNumber;
-          console.log( this.myName[0]+" "+"new position is"+" "+position);
-      }
-      else{
-        console.log( this.myName[0]+" "+"new position is"+" "+position);
-      }
+        console.log('YOU WIN and your position is' + ' ' + this.positions[this.memberChance - 1]);
+        this.setWinner(this.memberChance, this.roomToken);
+      } else{
+        console.log(this.names[this.memberChance - 1] + ' ' + 'new position is' + ' ' + this.positions[this.memberChance - 1]);
+        }
+      this.boardvals(this.positions[this.memberChance - 1], this.memberChance);
+    });
+
   }
 
- 
 
- 
-popup() {
-    const modal = this.modalController
-   
-    .create({
-      component: WinComponent,
-      cssClass: 'my-custom-modal-css',
-      showBackdrop: true,
-      backdropDismiss: true,
-      swipeToClose: true
-    })
-    .then(popElement => {
-      popElement.present(),
-        popElement.onDidDismiss().then(resp => {
-        });
+// here if you want only dice value which we get from backend then
+//  replace this.rollDiceChance() from this.boardService.diceRoll().
+  boardvals(currentPlayerPos, whichPlayer){
+    this.http.post<any>(`http://localhost:3000/board/${this.roomToken}`,
+    {memberChance : whichPlayer, position : currentPlayerPos})
+    .subscribe(resp => {
+      console.log(resp);
     });
-    this.WinnerAudio()
   }
-  //remove this popup1() if we have only one winner.
-  popup1() {
-    const modal = this.modalController
-    .create({
-      component: AllComponent,
-      cssClass: 'my-custom-modal-css',
-      showBackdrop: true,
-      backdropDismiss: true,
-      swipeToClose: true
-    })
-    .then(popElement => {
-      popElement.present(),
-        popElement.onDidDismiss().then(resp => {
-        });
+
+  setWinner(member, room){
+    this.http.post<any>('http://localhost:3000/setGameStats', {playerNo: member, roomid: room}).subscribe(resp => {
+      console.log(resp);
     });
-    this.WinnerAudio()
   }
-//   Calltoggle(){
-    
-//     function Calltoggle(){
-//       var blur = document.getElementById('blur');
-//       blur.classList.toggle('active')
-//     }
-//   }
+
+
+  // it shows the loading when you start the game.
+  async presentLoading() {
+    this.playAudio();
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'your game will start in few seconds',
+      duration: 14000
+    });
+    await loading.present();
+
+    console.log('Loading dismissed!');
+  }
+
+  playAudio(){
+    const audio = new Audio('../assets/starter.mpeg'); // audio play when we start the game
+    audio.play();
+  }
+  WinnerAudio(){
+    const audio = new Audio('../assets/winner.mp3.wav'); // audio play after wining of player.
+    audio.play();
+  }
+  diceAudio(){
+    const audio = new Audio('../assets/dice.mp3.wav'); // audio play on dice roll
+    audio.play();
+  }
+
+
+
+  popup() {
+      const modal = this.modalController
+      .create({
+        component: WinComponent,
+        cssClass: 'my-custom-modal-css',
+        showBackdrop: true,
+        backdropDismiss: true,
+        swipeToClose: true
+      })
+      .then(popElement => {
+        popElement.present(),
+          popElement.onDidDismiss().then(resp => {
+          });
+      });
+      this.WinnerAudio();
+    }
+    // remove this popup1() if we have only one winner.
+    popup1() {
+      const modal = this.modalController
+      .create({
+        component: AllComponent,
+        cssClass: 'my-custom-modal-css',
+        showBackdrop: true,
+        backdropDismiss: true,
+        swipeToClose: true
+      })
+      .then(popElement => {
+        popElement.present(),
+          popElement.onDidDismiss().then(resp => {
+          });
+      });
+      this.WinnerAudio();
+    }
+  //   Calltoggle(){
+  //     function Calltoggle(){
+  //       var blur = document.getElementById('blur');
+  //       blur.classList.toggle('active')
+  //     }
+  //   }
 }
-
- 
-
-
-
-
