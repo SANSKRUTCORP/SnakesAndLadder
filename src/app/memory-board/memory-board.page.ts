360317ce4099dec3ab5931c83d787a61d8470ac5
14bhuvaneshwari <bhuvaneshwarishaktawat@gmail.com>
2020-10-10 18:03:28 +0530

    modified event and had shuffle function

--- src/app/memory-board/memory-board.page.ts
+++ src/app/memory-board/memory-board.page.ts
@@ -8,7 +8,14 @@ import {memoryCards} from './memory-board.model';
 })
 export class MemoryBoardPage implements OnInit {
   timeLeft: number = 60;
+  currentIndex:any;
+  temporaryValue:any;
+  // arr:any=[1,1,2,2,3,3,4,4,5,5,6,6]
+  randomIndex:any;
+  name:any;
+  value_1:any;
   interval;
+  buttonValue:any;
   // first argument is for back-image and second argument is for front-image
   cards:memoryCards[]=[
     new memoryCards('https://tse3.mm.bing.net/th?id=OIP.fPwNkLmKV_nFUe13_oz1iQHaE8&pid=Api&P=0&w=234&h=157',
@@ -73,6 +80,38 @@ export class MemoryBoardPage implements OnInit {
                     'https://tse3.mm.bing.net/th?id=OIP.fPwNkLmKV_nFUe13_oz1iQHaE8&pid=Api&P=0&w=234&h=157'),
   ];
 constructor( public loadingController: LoadingController){}
+
+toggle(button) {
+  this.buttonValue = button.id;
+  console.log(this.buttonValue)
+}
+
+  
+shuffle(array:any) {
+  this.currentIndex = array.length, this.temporaryValue, this.randomIndex;
+ 
+  // While there remain elements to shuffle...
+  while (0 !== this.currentIndex) {
+
+    // Pick a remaining element...
+    this.randomIndex = Math.floor(Math.random() * this.currentIndex);
+    this.currentIndex -= 1;
+
+    // And swap it with the current element.
+    this.temporaryValue = array[this.currentIndex];
+    array[this.currentIndex] = array[this.randomIndex];
+    array[this.randomIndex] = this.temporaryValue;
+  }
+  this.name=array;
+  console.log(this.name);
+
+}
+
+click_1(value1:any){
+  console.log("hello " + value1);
+}
+
+
 startTimer() {
     this.interval = setInterval(() => {
       if(this.timeLeft > 0) {
@@ -104,8 +143,9 @@ startTimer() {
     audio.play();
   }
   ngOnInit() {
-    this.startLoading()
-    this.startTimer();
+    // this.startLoading()
+    // this.startTimer();
+    
   }
 
 }
