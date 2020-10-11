60e5dc211161e57615bdbe0b2ecf1c44a31c541e
Sheetal575 <sheetaldadh9@gmail.com>
2020-10-11 16:43:44 +0530

    in html i commented my code. set the code for ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,19 @@
-import { firebaseConfig } from './config';
+// import { firebaseConfig } from './config';
 
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
     production: false,
-    firebase : firebaseConfig,
+    firebase : {
+      apiKey: "AIzaSyAf3VcFwtdg7AeK4jDZNdVk4ZS2G_6L9Ck",
+      authDomain: "sanskrut-interns.firebaseapp.com",
+      databaseURL: "https://sanskrut-interns.firebaseio.com",
+      projectId: "sanskrut-interns",
+      storageBucket: "sanskrut-interns.appspot.com",
+      messagingSenderId: "514793907608",
+      appId: "1:514793907608:web:18f36505b9ebf0b533f7ed"
+    },
   };
   /*
    * For easier debugging in development mode, you can import the following file
