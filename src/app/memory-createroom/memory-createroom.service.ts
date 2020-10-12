149e30cc15333eeb1718b9a6d42df9757de2d3c3
Harsh <harsh.here17@gmail.com>
2020-10-12 17:51:17 +0530

    api calls and db reads on service files

--- src/app/memory-createroom/memory-createroom.service.ts
+++ src/app/memory-createroom/memory-createroom.service.ts
@@ -1,9 +1,81 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, NgZone } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthserviceService } from '../services/authservices.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemoryCreateroomService {
+  roomToken: any;
+  leader: any;
+  names: any;
+  truth: boolean;
+  state: any;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase,
+              private http: HttpClient,
+              public auth: AuthserviceService,
+              public zone: NgZone,
+              private router: Router) { }
+
+
+
+  listenPlayers(roomtok){
+    this.roomToken = roomtok;
+    this.zone = new NgZone({});
+    const ref = this.db.database.ref('memory/rooms/room_' + this.roomToken + '/players');
+
+    ref.on('value', (snapshot) => {
+      // this.roomToken = this.getRoomToken()
+      this.zone.run(() => {
+        for (let i = 1; i <= 2; i++){
+          const user = snapshot.child('player_' + i + '/name').val();
+          if (i === 1){
+            this.leader = user;
+          }
+          else{
+            this.names[i - 2] = user;
+          }
+        }
+      });
+    });
+
+  }
+
+  onClick(){
+    this.http.post<any>('/apis/mmry/setState', {roomid: this.roomToken}).subscribe(resp => {
+      console.log(resp);
+    });
+  }
+
+  checkButton(){
+    let loggedUser: any;
+    this.truth = false;
+    this.db.database.ref('memory/rooms/room_' + this.roomToken + '/players').once('value', snapshot => {
+      this.leader = snapshot.child('player_1/name').val();
+      this.auth.getUser().then(res => {
+        loggedUser = res.displayName;
+        console.log(loggedUser);
+        if (loggedUser === this.leader){
+          this.truth = true;
+          console.log(this.truth, loggedUser, this.leader);
+        }
+      });
+    });
+  }
+
+  navigator(){
+    this.db.database.ref('memory/rooms/room_' + this.roomToken).on('value', snapshot => {
+      this.state = snapshot.child('tempState').val();
+      if (this.state){
+        this.router.navigate(['/memory-board'], {
+          queryParams: {room : this.roomToken}
+        });
+      }
+    });
+  }
+
+
 }
