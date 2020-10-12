1085bb83d4716baeb5736bed72abf21cfd62847c
Harsh <harsh.here17@gmail.com>
2020-10-12 17:52:05 +0530

    import service files and required modules

--- src/app/memory-rooms/memory-join-room/memory-join-room.component.ts
+++ src/app/memory-rooms/memory-join-room/memory-join-room.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, snapshotChanges } from '@angular/fire/database';
+import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+import { AuthserviceService } from 'src/app/services/authservices.service';
 
 @Component({
   selector: 'app-memory-join-room',
@@ -8,10 +12,49 @@ import { ModalController } from '@ionic/angular';
 })
 export class MemoryJoinRoomComponent implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  roomNo: any;
+  uid: any;
+  constructor(private modalController: ModalController,
+              private router: Router,
+              private db: AngularFireDatabase,
+              private auth: AuthserviceService,
+              private http: HttpClient) { }
 
+
+  onClick(item){
+    let resp;
+    this.roomNo = item.value;
+    this.uid = localStorage.getItem('tempid');
+    const ref = this.db.database.ref('memory/rooms');
+    ref.once('value').then(snapshot => {
+      resp = snapshot.val();
+      if (resp['room_' + this.roomNo]){
+
+        this.auth.getUser().then(res => {
+          this.http.post<any>('/apis/joinroom',
+          // this.http.post<any>(this.utils.GetServerHost() + '/apis/joinroom',
+          {
+            enterid : this.roomNo,
+            entername : res.displayName,
+            uid : this.uid
+          }).subscribe(response => {
+            if (response){
+              this.router.navigate(['/memory-createroom'], {
+                queryParams: {room : this.roomNo}
+              });
+            } else {
+              alert('Max players in Room!');
+            }
+          });
+        });
+      }
+      else{
+        alert('Room Number does not exists! Try again...');
+      }
+    });
+
+  }
   ngOnInit() {}
-
   CloseModal()
   {
     this.modalController.dismiss();
