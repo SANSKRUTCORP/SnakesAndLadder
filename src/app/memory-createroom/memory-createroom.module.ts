9f7585f86cbd0359cc76d89d97e47471b91035da
Harsh <harsh.here17@gmail.com>
2020-10-12 17:50:12 +0530

    required modules and authgaurd import

--- src/app/memory-createroom/memory-createroom.module.ts
+++ src/app/memory-createroom/memory-createroom.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
-
+import { MemoryCreateroomService } from './memory-createroom.service';
 import { MemoryCreateroomPageRoutingModule } from './memory-createroom-routing.module';
-
+import { TokenInterceptor } from '../services/auth.interceptor';
 import { MemoryCreateroomPage } from './memory-createroom.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ClipboardModule,
+    HttpClientModule,
     IonicModule,
     MemoryCreateroomPageRoutingModule
   ],
-  declarations: [MemoryCreateroomPage]
+  declarations: [MemoryCreateroomPage],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass : TokenInterceptor,
+      multi : true
+    },
+    MemoryCreateroomService
+  ]
 })
 export class MemoryCreateroomPageModule {}
