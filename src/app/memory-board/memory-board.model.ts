afe0c9902fc4aabcd1af76bcdb4bbdbeab0550de
14bhuvaneshwari <bhuvaneshwarishaktawat@gmail.com>
2020-10-12 16:49:34 +0530

    added the 2 flip functions and modified the match function

--- src/app/memory-board/memory-board.model.ts
+++ src/app/memory-board/memory-board.model.ts
@@ -1,6 +1,8 @@
 export class memoryCards {
     public frontImage: string;
     public backImage: string;
+  flipped: any;
+  flipped1: boolean;
     constructor(backImage: string, frontImage: string){
         this.frontImage = frontImage;
         this.backImage = backImage;
